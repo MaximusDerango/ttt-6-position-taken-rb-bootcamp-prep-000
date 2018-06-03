5d86c23066505b01b14c3bdbb07d8182ab3708fb
Maxim Dalton <maximcdalton@gmail.com>
2018-06-03 05:06:04 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, pos)
-  not [" ", "", nil].include?(board[pos])
+  #not [" ", "", nil].include?(board[pos])
+  not [" ", "", nil].collect{|x| x == board[pos]}.any?
 end
